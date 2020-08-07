236204658f24717f0ec2bbad0c02fefd3519a12d
devyyj <devyyj@gmail.com>
2020-08-07 13:36:26 +0900

    카카카 지도 API 작업 시작

--- pages/createCard.vue
+++ pages/createCard.vue
@@ -67,6 +67,8 @@
           placeholder="운동 장소를 입력하세요."
           :state="locationState"
         ></b-form-input>
+        <!-- 지도 -->
+        <div id="map" style="width: 500px; height: 400px;"></div>
       </b-form-group>
 
       <b-form-row>
@@ -206,6 +208,17 @@ export default {
     } catch (error) {
       alert(error)
     }
+  },
+  mounted() {
+    const container = document.getElementById('map') // 지도를 담을 영역의 DOM 레퍼런스
+    const options = {
+      // 지도를 생성할 때 필요한 기본 옵션
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표.
+      level: 3, // 지도의 레벨(확대, 축소 정도)
+    }
+
+    const map = new window.kakao.maps.Map(container, options) // 지도 생성 및 객체 리턴
+    console.log(map)
   },
   methods: {
     async onSubmit(evt) {
